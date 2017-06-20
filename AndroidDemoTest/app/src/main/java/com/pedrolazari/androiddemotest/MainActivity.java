56bc05e3d61a13b2b52080132e423179540d5019
Pedro <plazari96@gmai.com>
2017-06-19 23:54:30 -0300

    Iniciando o Curso de ANDROID

---
+++ AndroidDemoTest/app/src/main/java/com/pedrolazari/androiddemotest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pedrolazari.androiddemotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
